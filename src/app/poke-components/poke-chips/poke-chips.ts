6598d311afc99169ae4d05b02a64b6c44cf6bddd
Max <korniykom@gmail.com>
2025-07-26 01:07:16 +0300

    feat: add sorting chips

---
+++ src/app/poke-components/poke-chips/poke-chips.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { MatChipsModule } from "@angular/material/chips";
+@Component({
+  selector: "app-poke-chips",
+  imports: [MatChipsModule],
+  templateUrl: "./poke-chips.html",
+  styleUrl: "./poke-chips.scss",
+})
+export class PokeChips {}
