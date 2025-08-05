d41893f543290c964ecac7d72ccfb342c77399d3
Max <korniykom@gmail.com>
2025-08-06 00:50:11 +0300

    feat: add offline\online switch

--- src/app/poke-components/poke-chips/poke-chips.component.ts
+++ src/app/poke-components/poke-chips/poke-chips.component.ts
@@ -1,14 +1,29 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output, signal } from "@angular/core";
 import { MatChipsModule } from "@angular/material/chips";
+import {
+  MatSlideToggle,
+  MatSlideToggleModule,
+} from "@angular/material/slide-toggle";
 @Component({
   selector: "app-poke-chips",
-  imports: [MatChipsModule],
+  imports: [MatChipsModule, MatSlideToggleModule],
   templateUrl: "./poke-chips.component.html",
   styleUrl: "./poke-chips.component.scss",
 })
 export class PokeChips {
   @Input() selected: string | null = null;
   @Output() sortChange = new EventEmitter<string>();
+
+  isOnline = signal(navigator.onLine);
+
+  constructor() {
+    window.addEventListener("online", () => {
+      this.isOnline.set(true);
+    });
+    window.addEventListener("offline", () => {
+      this.isOnline.set(false);
+    });
+  }
 
   onChipClick(value: string) {
     this.sortChange.emit(value);
