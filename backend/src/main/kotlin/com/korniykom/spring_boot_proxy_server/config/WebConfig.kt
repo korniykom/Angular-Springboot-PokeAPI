7bd9756ff275c78cea1648d7b1e8b21188ffa95b
Max <korniykom@gmail.com>
2025-08-18 13:03:05 +0300

    feat: add configuration to serve angular frontend from spring boot

--- backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/config/WebConfig.kt
+++ backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/config/WebConfig.kt
@@ -1,21 +1,13 @@
 package com.korniykom.spring_boot_proxy_server.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig {
-
-    @Bean
-    fun corsConfigurer(): WebMvcConfigurer {
-        return object : WebMvcConfigurer {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200")
-                    .allowedMethods("GET")
-            }
-        }
+class WebConfig : WebMvcConfigurer {
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/poke-api/").setViewName("forward:/poke-api/index.html")
+        registry.addViewController("/poke-api/{spring:[^\\.]*}").setViewName("forward:/poke-api/index.html")
     }
 }
