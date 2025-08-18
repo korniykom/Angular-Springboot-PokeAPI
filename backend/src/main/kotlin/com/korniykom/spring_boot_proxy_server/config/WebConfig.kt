23b38913888688beb64ae68e40033e5e4d3c2624
Max <korniykom@gmail.com>
2025-08-18 16:49:29 +0300

    feat: add root path pointing to pokeapi frontend

--- backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/config/WebConfig.kt
+++ backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/config/WebConfig.kt
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("").setViewName("forward:/poke-api/index.html")
+        registry.addViewController("/").setViewName("forward:/poke-api/index.html")
         registry.addViewController("/poke-api").setViewName("forward:/poke-api/index.html")
         registry.addViewController("/poke-api/").setViewName("forward:/poke-api/index.html")
         registry.addViewController("/poke-api/{spring:[^\\.]*}").setViewName("forward:/poke-api/index.html")
