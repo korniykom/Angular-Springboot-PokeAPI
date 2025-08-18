1940976878363bd2a33c64429447ff9604a2cf87
Max <korniykom@gmail.com>
2025-08-18 16:23:07 +0300

    fix: add /poke-api route to WebConfiguration

--- backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/config/WebConfig.kt
+++ backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/config/WebConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/poke-api").setViewName("forward:/poke-api/index.html")
         registry.addViewController("/poke-api/").setViewName("forward:/poke-api/index.html")
         registry.addViewController("/poke-api/{spring:[^\\.]*}").setViewName("forward:/poke-api/index.html")
     }
