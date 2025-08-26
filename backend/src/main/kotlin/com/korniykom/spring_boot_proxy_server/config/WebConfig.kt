a87cadb3e7bbe28d23c2eb7c00274b2d1a81e7d5
Max <korniykom@gmail.com>
2025-08-27 01:55:17 +0300

    feat: add cors policies for local testing

--- backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/config/WebConfig.kt
+++ backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/config/WebConfig.kt
@@ -1,11 +1,18 @@
 package com.korniykom.spring_boot_proxy_server.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:4200")
+            .allowedMethods("GET")
+    }
+
     override fun addViewControllers(registry: ViewControllerRegistry) {
         registry.addViewController("").setViewName("forward:/poke-api/index.html")
         registry.addViewController("/").setViewName("forward:/poke-api/index.html")
