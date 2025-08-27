a8912adc9820252bf7db626272aa1251ec35cca9
Max <korniykom@gmail.com>
2025-08-27 22:04:30 +0300

    fix: make baseUrl local variable instead of global

--- backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/SpringBootProxyServerApplication.kt
+++ backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/SpringBootProxyServerApplication.kt
@@ -2,8 +2,10 @@ package com.korniykom.spring_boot_proxy_server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableAsync
 class SpringBootProxyServerApplication
 
 fun main(args: Array<String>) {
--- backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/service/PokeService.kt
+++ backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/service/PokeService.kt
@@ -6,16 +6,17 @@ import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
 
-const val BASE_URL = "https://pokeapi.co/api/v2/pokemon"
-
 @Service
-class PokeService(private val restTemplate: RestTemplate) {
+class PokeService(
+    private val restTemplate: RestTemplate,
+) {
+    private val baseUrl: String = "https://pokeapi.co/api/v2/pokemon"
 
     fun getPokemon(nameOrId: String): Pokemon {
 
         val locationResponse =
-            restTemplate.getForObject<Array<LocationResponse>>("$BASE_URL/${nameOrId}/encounters")
-        val pokemonResponse = restTemplate.getForObject<Pokemon>("$BASE_URL/$nameOrId")
+            restTemplate.getForObject<Array<LocationResponse>>("$baseUrl/${nameOrId}/encounters")
+        val pokemonResponse = restTemplate.getForObject<Pokemon>("$baseUrl/$nameOrId")
 
         return pokemonResponse.copy(
             location = locationResponse.firstOrNull()?.location_area?.name
