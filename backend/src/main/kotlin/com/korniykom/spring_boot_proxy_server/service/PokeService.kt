9c4ac54661baefda852cadcf6d3b17d49ec0f6ad
Max <korniykom@gmail.com>
2025-09-03 19:31:45 +0300

    fix: make requests asynchronous

--- backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/service/PokeService.kt
+++ backend/src/main/kotlin/com/korniykom/spring_boot_proxy_server/service/PokeService.kt
@@ -2,23 +2,47 @@ package com.korniykom.spring_boot_proxy_server.service
 
 import Pokemon
 import com.korniykom.spring_boot_proxy_server.model.LocationResponse
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
+import org.springframework.web.reactive.function.client.WebClient
+import kotlin.coroutines.coroutineContext
 
-const val BASE_URL = "https://pokeapi.co/api/v2/pokemon"
 
 @Service
-class PokeService(private val restTemplate: RestTemplate) {
+class PokeService(
+    private val webClient: WebClient.Builder,
+    @Value("\${poke.api.base.url}") private val baseUrl: String
+) {
+    suspend fun getPokemon(nameOrId: String): Pokemon = coroutineScope {
+        val client = webClient.baseUrl(baseUrl).build()
 
-    fun getPokemon(nameOrId: String): Pokemon {
+        val locationDeferred = async {
+            client.get()
+                .uri("/$nameOrId/encounters")
+                .retrieve()
+                .bodyToMono(Array<LocationResponse>::class.java)
+                .awaitSingleOrNull()
+        }
 
-        val locationResponse =
-            restTemplate.getForObject<Array<LocationResponse>>("$BASE_URL/${nameOrId}/encounters")
-        val pokemonResponse = restTemplate.getForObject<Pokemon>("$BASE_URL/$nameOrId")
+        val pokemonDeferred = async {
+            client.get()
+                .uri("/$nameOrId")
+                .retrieve()
+                .bodyToMono(Pokemon::class.java)
+                .awaitSingleOrNull()
+        }
 
-        return pokemonResponse.copy(
-            location = locationResponse.firstOrNull()?.location_area?.name
+        val locationResponse = locationDeferred.await()
+        val pokemonResponse = pokemonDeferred.await()
+            ?: throw RuntimeException("Pokemon not found: $nameOrId")
+
+        pokemonResponse.copy(
+            location = locationResponse?.firstOrNull()?.location_area?.name
         )
     }
 }
